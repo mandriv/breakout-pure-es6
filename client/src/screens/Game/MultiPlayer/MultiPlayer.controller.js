65da25877dd7b7759fb3f107b9f4832544653646
mandriv <wojciech.cichoradzki@gmail.com>
2018-03-11 14:53:32 +0000

    Add client socket handling

--- client/src/screens/Game/MultiPlayer/MultiPlayer.controller.js
+++ client/src/screens/Game/MultiPlayer/MultiPlayer.controller.js
@@ -2,17 +2,33 @@ import GameController from '../Game.controller';
 
 export default class MultiPlayerController extends GameController {
 
+  constructor(router, socket, roomID) {
+    super(router);
+    this.socket = socket;
+    this.roomID = roomID;
+  }
+
   init = () => {
     this.initialize();
 
     this.model.loadMultiPlayer();
 
+    this.socket.on('angle', (angle) => {
+      this.model.paddles[1].angle = angle;
+    });
+
     window.addEventListener('deviceorientation', (ev) => {
+      let angle;
       if (window.innerHeight > window.innerWidth) {
-        this.model.paddles[0].angle = Math.round(ev.gamma);
+        angle = Math.round(ev.gamma);
       } else {
-        this.model.paddles[0].angle = Math.round(ev.beta);
+        angle = Math.round(ev.beta);
       }
+      this.model.paddles[0].angle = angle;
+      this.socket.emit('angle', {
+        roomID: this.roomID,
+        angle,
+      });
     }, true);
 
     this.start();
